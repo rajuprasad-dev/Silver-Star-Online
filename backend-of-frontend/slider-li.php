1feeacbbb9c99bcd9339235a67e5b6a775a051fa
Raju Prasad <rk24052000@gmail.com>
2024-02-14 16:39:49 +0530

    Fix added

--- backend-of-frontend/slider-li.php
+++ backend-of-frontend/slider-li.php
@@ -1,17 +1,5 @@
 <?php
-// Assuming you have a database connection established
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "silverstaronline";
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+include_once "conn.php";
 
 // Query to fetch data from the site_slider table
 $sql = "SELECT * FROM site_slider";
