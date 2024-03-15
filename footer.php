84c3f7ab595242444d741ff41ab113885ae6b7e0
Raju <officialrajuprasad@gmail.com>
2024-03-15 18:42:43 +0530

    Contact form details added

--- footer.php
+++ footer.php
@@ -1,12 +1,15 @@
 <footer class="fdb-block footer-large px-5 py-5 mt-5" style="background-color:#f6f4f2;">
     <div class="row">
         <div class="col-sm-6 text-center">
-            <h4><b>Silver Star</b></h4>
+            <div class="logo-part d-flex justify-content-center align-items-center mb-3">
+                <img src="./assets/img/logo.png" alt="Logo" width="60px" class="mr-3 mb-3">
+                <h4><b>Silver Star</b></h4>
+            </div>
             <nav class="nav flex-row justify-content-center">
-                <a class="mb-2 custom-link mx-2"><i style="font-size:24px" class="fa fa-cc-visa"></i></a>
-                <a class=" mb-2 custom-link mx-2"><i style="font-size:24px" class="fa fa-cc-mastercard"></i></a>
-                <a class=" mb-2 custom-link mx-2"><i style="font-size:24px" class="fa fa-amazon"></i></a>
-                <a class=" mb-2 custom-link mx-2"><i style="font-size:24px" class="fa fa-cc-amex"></i></a>
+                <a class="mb-2 footer-custom-link mx-2"><i style="font-size:24px" class="fa fa-cc-visa"></i></a>
+                <a class=" mb-2 footer-custom-link mx-2"><i style="font-size:24px" class="fa fa-cc-mastercard"></i></a>
+                <a class=" mb-2 footer-custom-link mx-2"><i style="font-size:24px" class="fa fa-amazon"></i></a>
+                <a class=" mb-2 footer-custom-link mx-2"><i style="font-size:24px" class="fa fa-cc-amex"></i></a>
             </nav>
         </div>
 
@@ -14,18 +17,18 @@
             <div class="row align-items-top text-center">
                 <div class="col-6 col-md-4 mt-5 mt-sm-0  text-sm-left">
                     <nav class="nav flex-column">
-                        <a class="mb-2" href="./"><span class="custom-link">Home</span></a>
-                        <a class="mb-2" href="./shop"><span class="custom-link">Shop</span></a>
-                        <a class="mb-2" href="./about-us"><span class="custom-link">About Us</span></a>
-                        <a class="mb-2" href="./contact-us"><span class="custom-link">Contact Us</span></a>
+                        <a class="mb-2" href="./"><span class="footer-custom-link">Home</span></a>
+                        <a class="mb-2" href="./shop"><span class="footer-custom-link">Shop</span></a>
+                        <a class="mb-2" href="./about-us"><span class="footer-custom-link">About Us</span></a>
+                        <a class="mb-2" href="./contact-us"><span class="footer-custom-link">Contact Us</span></a>
                     </nav>
                 </div>
 
                 <div class="col-6 col-md-4 mt-5 mt-sm-0 text-sm-left">
                     <nav class="nav flex-column">
-                        <a class="mb-2 custom-link" href="./privacy-policy">Privacy Policy</a>
-                        <a class="mb-2 custom-link" href="./refund-policy">Refund Policy</a>
-                        <a class="mb-2 custom-link" href="./terms-and-conditons">Terms & Conditions</a>
+                        <a class="mb-2 footer-custom-link" href="./privacy-policy">Privacy Policy</a>
+                        <a class="mb-2 footer-custom-link" href="./refund-policy">Refund Policy</a>
+                        <a class="mb-2 footer-custom-link" href="./terms-and-conditons">Terms & Conditions</a>
                     </nav>
                 </div>
             </div>
